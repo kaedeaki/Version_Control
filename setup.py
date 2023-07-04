e7e3472666eb650b603bbf6c8bc522e0102d5b0e
kaedeaki <madokafuji@yahoo.com>
2023-07-04 18:18:47 -0400

    third modify

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setuptools.setup(
 	classifiers=[
         	"Programming Language :: Python :: 3",
         	"License :: OSI Approved :: MIT License",
-		"Operating System :: OS Indipendent",
+		"Operating System :: OS Independent",
     ],
     packages=setuptools.find_packages(),
 	python_requires='>=3.6',
